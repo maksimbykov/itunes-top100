9493b1316e6906ed7f60e0fe3c50b754df665662
Max B <maksym.bykov@gmail.com>
2022-06-20 01:12:59 +0300

    added search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { ListComponent } from './list/list.component';
 
 @NgModule({
@@ -16,7 +18,8 @@ import { ListComponent } from './list/list.component';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { Item } from '../models/item';
 import { ItunesService } from '../services/itunes.service';
 
@@ -10,15 +11,30 @@ import { ItunesService } from '../services/itunes.service';
 export class ListComponent implements OnInit {
 
   constructor(private itunesService: ItunesService) { }
-  
+
+  @Input()
+  albumName!: FormControl;
+
   albumsList!: Item[];
+  cachedList!: Item[];
 
   ngOnInit(): void {
-    this.itunesService.getAlbumsInfo().subscribe(
-      res => {
-        this.albumsList = res;
-      }
-    );
+    this.itunesService
+      .getAlbumsInfo()
+      .subscribe(
+        res => this.albumsList = this.cachedList = res
+      );
+
+    this.albumName
+      .valueChanges.subscribe(
+        value => {
+          if (value) {
+            this.albumsList = this.cachedList.filter(i => i.title.label.toLowerCase().includes(value.toLowerCase()))
+          } else {
+            this.albumsList = this.cachedList;
+          }
+        }
+      );
   }
 
 }
