b7486c0e1410c7f62a9a6474012055ec87556ae6
Max B <maksym.bykov@gmail.com>
2022-06-20 12:05:53 +0300

    AT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,26 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA-0vwWkmjcxByi0Q9dYFic7FuF5ENAZiY",
+  authDomain: "itunes-top100.firebaseapp.com",
+  projectId: "itunes-top100",
+  storageBucket: "itunes-top100.appspot.com",
+  messagingSenderId: "192119451040",
+  appId: "1:192119451040:web:f835cef4c96a26769d88c5",
+  measurementId: "G-TZ2XNE7BH3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 @Component({
   selector: 'app-root',
